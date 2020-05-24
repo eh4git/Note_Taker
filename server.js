ca83328764eeddbc9b02dafc0f902bf89dee4be8
erik hirsch <ehirsch760@gmail.com>
2020-05-23 19:47:28 -0700

    first api route get sending correct response

--- server.js
+++ server.js
@@ -1,67 +1,63 @@
 // Dependencies
 const express = require("express")
 const fs = require("fs");
-const app = express();
 const path = require("path");
+const app = express();
 // Set our port to production ready port or 8080
 const PORT = process.env.PORT || 8080;
+//Set static route
 app.use(express.static('public'))
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 //API Routes
 //Get saved notes from db.json
-app.get("/api/notes", function(req, res){
-  return res.json(req.body);
+app.get("/api/notes", function (req, res) {
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) throw err
+    jsonData = JSON.parse(data)
+    return res.json(jsonData);
+  })
+  
 })
-app.get("/api/notes", function(req, res) {
-  fs.readFile('/etc/passwd', (err, data) => {
-    if (err){
-      throw err;
-    console.log(data);
-  } else {
-    console.log(data)
-  }
-  });
-  });
+// app.get("/api/notes", function (req, res) {
+//   fs.readFile('db.json', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//     return res.json("db.json");
+//   });
+// });
 
-// Post notes from json 
-app.post("/api/notes", function(req, res) {
+// Write note on db.json 
+app.post("/api/notes", function (req, res) {
   let newNote = req.body
-  fs.appendFile('db.json', newNote, (err) => {
-    if (err) throw err;
-    console.log('The "newNote" was appended to file!');
-  });
+  fs.writeFile("db.json", newNote, (err) => {
+    if (err) return console.log(err);
+
+  })
 });
 
-// Delete notes from json
+// Delete note from db.json
 // app.delete("/api/notes", function(req, res) {
 //   
 // });
 
 // HTML Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.js"));
+  res.sendFile(path.join(__dirname, "/public/index.js"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
+// Catch all Html route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.js"));
+  res.sendFile(path.join(__dirname, "/public/index.js"));
 });
 
 // The catch all route I.E. * has to be at the bottom of your routes. Otherwise, it'll break everything.
-
-
-
-
-
-
-
-
-
 
 
 app.listen(PORT, () => {
